380a481d046015658803aa144dbd136dc3eb92b3
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-21 18:31:42 +0300

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,31 +9,46 @@ import {
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegistrationForm from "./components/RegistrationForm";
+
 import LoginForm from "./components/LoginForm";
-import Dashboard from "./components/Dashboard";
+// import DashboardComponent from "./components/Dashboard";
 import ProfilePage from "./components/ProfilePage";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import ActivityFeed from "./components/ActivityFeed";
 import TaskDashboard from "./components/TaskDashboard";
-import React from "react";
+import { useAuth } from "./hooks/useAuth";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/register" element={<RegistrationForm />} />
+//         <Route path="/login" element={<LoginForm />} />
+//         <Route path="/taskdashboard" element={<TaskDashboard/>} />
+//         <Route path="/profile" element={<ProfilePage />} />
+//         <Route path="/tasks/create" element={<TaskForm />} />
+//         <Route path="/tasks" element={<TaskList />} />
+//         <Route path="/activity" element={<ActivityFeed />} />
+//         <Route path="/" element={<Navigate to="/dashboard" />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
 
+// export default App;
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/taskdashboard" element={<TaskDashboard/>} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/tasks/create" element={<TaskForm />} />
-        <Route path="/tasks" element={<TaskList />} />
-        <Route path="/activity" element={<ActivityFeed />} />
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-      </Routes>
-    </Router>
+    <div>
+      <RegistrationForm />
+      <LoginForm />
+      <DashboardComponent />
+      <ProfilePage />
+      <TaskForm />
+      <TaskList />
+    </div>
   );
 }
 
